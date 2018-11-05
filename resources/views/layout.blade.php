f63a818b60bdec4e9db87a77a3c4158210412c8b
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-11-05 14:30:24 +0100

    Add files via upload

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,14 @@
 @include('head')
 @include('nav')
 
+<!--------- Input errors -------------->
+                    @if (count($errors) > 0)
+                          @include('input-errors', ["errors" => $errors])
+                     @endif   
+                    
+@if(Session::has('contact-status') && Session::get('contact-status') == "ok")
+			 <center><div class="alert alert-success">Message sent!  We will respond shortly. </div></center>
+			@endif
 
 @yield('slider')
 
