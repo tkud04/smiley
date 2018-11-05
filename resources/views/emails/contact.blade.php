5e3aea2888d83be15f2ef05f3f4d0887a0a4b65e
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-11-05 14:04:41 +0100

    Added contact email blade

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,8 @@
+<center><h3>You have a new Message! </h3></center>
+
+<center>
+<p><strong>Name: </strong>{{$name}}</p><br>
+<p><strong>Email: </strong>{{$email}}</p><br>
+<strong>Message: </strong><br><blockquote>{{$msg}}</blockquote><br>
+</center><br>
+	
